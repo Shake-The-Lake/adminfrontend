ff9a8249e1c1fc3fde9af9efbe0184ee42bbd6a5
Burger Alain (IT-PTR-CEN2-YPT) <alain_nicolas.burger@sbb.ch>
2024-05-18 14:23:26 +0200

    feat: created dynamical dialog component and implemented reusable structure for forms and dialogs

---
+++ src/components/dialog/dialog.tsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import {
+	Dialog,
+	DialogClose,
+	DialogContent,
+	DialogDescription,
+	DialogFooter,
+	DialogHeader,
+	DialogTitle,
+	DialogTrigger,
+} from '../../components/ui/dialog';
+import {Button} from '../../components/ui/button';
+
+export type StlDialogProps = {
+	title: string;
+	description: string;
+	triggerLabel: string;
+	onSubmit: () => void;
+	children: React.ReactNode;
+};
+
+const StlDialog: React.FC<StlDialogProps> = ({
+	title,
+	description,
+	triggerLabel,
+	onSubmit,
+	children,
+}) => {
+	return (
+		<Dialog>
+			<DialogTrigger asChild>
+				<Button className="w-full text-center">{triggerLabel}</Button>
+			</DialogTrigger>
+			<DialogContent className="sm:max-w-md h-full">
+				<DialogHeader>
+					<DialogTitle>{title}</DialogTitle>
+					<DialogDescription>{description}</DialogDescription>
+				</DialogHeader>
+				<div className="grow-0 overflow-auto min-h-32">{children}</div>
+				<DialogFooter className="justify-end items-end">
+					<DialogClose asChild>
+						<Button type="button" variant="secondary">
+							Cancel
+						</Button>
+					</DialogClose>
+					<Button type="submit" onClick={onSubmit}>
+						Save
+					</Button>
+				</DialogFooter>
+			</DialogContent>
+		</Dialog>
+	);
+};
+
+export default StlDialog;
---
+++ src/components/forms/event.tsx
@@ -0,0 +1,116 @@
+import React from 'react';
+import {SubmitHandler, useForm} from 'react-hook-form';
+import {z} from 'zod';
+import {zodResolver} from '@hookform/resolvers/zod';
+import {Form, FormControl, FormField, FormItem, FormLabel} from '../ui/form';
+import {Input} from '../ui/input';
+
+const formSchema = z.object({
+	title: z.string().min(5).max(20),
+	description: z.string(),
+	date: z.string(),
+	startedAt: z.string(),
+	customerOnlyTime: z.string(),
+	location: z.string(),
+});
+
+type EventFormProps = {
+	onSubmit: SubmitHandler<z.infer<typeof formSchema>>;
+};
+
+const EventForm: React.FC<EventFormProps> = ({onSubmit}) => {
+	const form = useForm<z.infer<typeof formSchema>>({
+		resolver: zodResolver(formSchema),
+		mode: 'onChange',
+	});
+
+	return (
+		<Form {...form}>
+			<form className="p-1" onSubmit={form.handleSubmit(onSubmit)}>
+				<FormField
+					name="title"
+					control={form.control}
+					render={({field}) => (
+						<FormItem>
+							<FormLabel>Title</FormLabel>
+							<FormControl>
+								<Input
+									placeholder="Sommer Event 2024"
+									{...field}
+									className="input"
+								/>
+							</FormControl>
+						</FormItem>
+					)}
+				/>
+				<FormField
+					name="description"
+					control={form.control}
+					render={({field}) => (
+						<FormItem>
+							<FormLabel>Description</FormLabel>
+							<FormControl>
+								<Input
+									placeholder="Shake the Lake 2024"
+									{...field}
+									className="input"
+								/>
+							</FormControl>
+						</FormItem>
+					)}
+				/>
+				<FormField
+					name="location"
+					control={form.control}
+					render={({field}) => (
+						<FormItem>
+							<FormLabel>Location</FormLabel>
+							<FormControl>
+								<Input {...field} className="input" />
+							</FormControl>
+						</FormItem>
+					)}
+				/>
+				<FormField
+					name="date"
+					control={form.control}
+					render={({field}) => (
+						<FormItem>
+							<FormLabel>Date</FormLabel>
+							<FormControl>
+								<Input type="datetime-local" {...field} className="input" />
+							</FormControl>
+						</FormItem>
+					)}
+				/>
+				<FormField
+					name="startedAt"
+					control={form.control}
+					render={({field}) => (
+						<FormItem>
+							<FormLabel>Internal Start</FormLabel>
+							<FormControl>
+								<Input type="datetime-local" {...field} className="input" />
+							</FormControl>
+						</FormItem>
+					)}
+				/>
+				<FormField
+					name="customerOnlyTime"
+					control={form.control}
+					render={({field}) => (
+						<FormItem>
+							<FormLabel>External Start</FormLabel>
+							<FormControl>
+								<Input type="datetime-local" {...field} className="input" />
+							</FormControl>
+						</FormItem>
+					)}
+				/>
+				<button type="submit" style={{display: 'none'}} />
+			</form>
+		</Form>
+	);
+};
+
+export default EventForm;
