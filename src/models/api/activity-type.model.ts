8e16bb7d6d19252ad585297387bd9e60ac4c0159
7imonn <timon.vandennobelen@novu.ch>
2024-05-18 13:42:17 +0200

    feat: add models of activity types

---
+++ src/models/api/activity-type.model.ts
@@ -0,0 +1,10 @@
+import {type LocalizedStringDto} from './localized-string';
+
+export type ActivityTypeDto = {
+	id: number;
+	name: LocalizedStringDto;
+	description: LocalizedStringDto;
+	checklist: LocalizedStringDto;
+	icon: string;
+	eventId: number;
+};
