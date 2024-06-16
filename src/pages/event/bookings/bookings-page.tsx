a8905ae7d32f3d9e5c0dd81ba7da5b9a6cf2c91c
Burger Alain (IT-PTR-CEN2-YPT) <alain_nicolas.burger@sbb.ch>
2024-06-16 15:22:26 +0200

    feat: added skeleton for data table component for bookings

--- src/pages/event/bookings/bookings-page.tsx
+++ src/pages/event/bookings/bookings-page.tsx
@@ -1,16 +1,46 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {useTranslation} from 'react-i18next';
-import {useLocation} from 'react-router-dom';
+import {DataTable} from '../../../components/data-table/data-table';
+import {BookingDto, columns} from '../../../models/api/booking.model';
 
 const BookingsPage: React.FC = () => {
 	const {t} = useTranslation();
-	const location = useLocation();
+	const [data, setData] = useState<BookingDto[]>([]);
+
+	async function getData(): Promise<BookingDto[]> {
+		return [
+			{
+				id: 1,
+				isRider: true,
+				isManual: false,
+				pagerNumber: 123,
+				person: 'John Doe',
+				timeSlot: {
+					id: 1,
+					fromTime: new Date().toISOString(),
+					untilTime: new Date().toISOString(),
+					boatId: 1,
+					bookingIds: new Set([1]),
+				},
+			},
+		];
+	}
+
+	useEffect(() => {
+		const fetchData = async () => {
+			const result = await getData();
+			setData(result);
+		};
+		fetchData();
+	}, []);
 
 	return (
 		<>
-			<div className="flex flex-col items-center py-24">
-				<h1 className="text-2xl font-bold mb-10">{t('bookings')}</h1>
-				<h4>{t('welcomeMessage')}</h4>
+			<div className="flex flex-col items-center">
+				<div className="w-full mb-8 flex flex-col justify-start">
+					<h1>Bookings</h1>
+				</div>
+				<DataTable columns={columns} data={data} />
 			</div>
 		</>
 	);
