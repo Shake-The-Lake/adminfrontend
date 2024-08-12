e5580be5283c1e43e50fee3c0948ae2f9c9b7d6e
7imonn <timon.vandennobelen@novu.ch>
2024-08-12 13:47:42 +0200

    load data in shedule page

--- src/pages/event/schedule/schedule-page.tsx
+++ src/pages/event/schedule/schedule-page.tsx
@@ -1,16 +1,21 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {useTranslation} from 'react-i18next';
 import {useLocation} from 'react-router-dom';
+import {type BoatDto} from '../../../models/api/boat.model';
 
 const SchedulePage: React.FC = () => {
 	const {t} = useTranslation();
 	const location = useLocation();
 
+	const [boats, setBoats] = useState<BoatDto[]>([]);
+	console.log('boats', boats);
+	const [loading, setLoading] = useState(true);
+
 	return (
 		<>
 			<div className="flex flex-col items-center py-24">
 				<h1 className="text-2xl font-bold mb-10">{t('schedule')}</h1>
-				<h4>{t('welcomeMessage')}</h4>
+				<h4>hiiii</h4>
 			</div>
 		</>
 	);
