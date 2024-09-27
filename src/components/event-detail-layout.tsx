aa276141859e4c67fd8e01e0af55c403b78efc1a
7imonn <timon.vandennobelen@novu.ch>
2024-09-27 21:49:05 +0200

    feat: add spacing to header

--- src/components/event-detail-layout.tsx
+++ src/components/event-detail-layout.tsx
@@ -42,7 +42,7 @@ const EventDetailLayout: React.FC = () => {
 				<div className="flex flex-col">
 					<HeaderEvent />
 					<main className="flex flex-1 max-w-[100vw] flex-col gap-4 px-4 lg:gap-6 lg:py-6 lg:px-10 overflow-auto">
-						<h1 className="heading-m-upper text-primary">{event?.title}</h1>
+						<h1 className="mt-1 heading-m-upper text-primary">{event?.title}</h1>
 						<Outlet />
 					</main>
 					<Footer />
