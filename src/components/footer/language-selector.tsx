952d0b84a785d4b383feb62bbeab130396d10258
7imonn <timon.vandennobelen@novu.ch>
2024-09-28 20:28:13 +0200

    ƒix: bug after reload of page lang switcher was on de and i18n on en

--- src/components/footer/language-selector.tsx
+++ src/components/footer/language-selector.tsx
@@ -1,25 +1,28 @@
-import React, {useState} from 'react';
-import {Separator} from '../ui/separator';
-import {ch, de, en} from '../../constants';
-import {ToggleGroup, ToggleGroupItem} from '../ui/toggle-group';
-import {useTranslation} from 'react-i18next';
+import React, { useEffect, useState } from 'react';
+import { Separator } from '../ui/separator';
+import { ch, de, en } from '../../constants';
+import { ToggleGroup, ToggleGroupItem } from '../ui/toggle-group';
+import { useTranslation } from 'react-i18next';
 
 const LanguageSelector: React.FC = () => {
-	const [language, setLanguage] = useState(() => {
-		const savedLanguage = localStorage.getItem('selectedLanguage');
-		return savedLanguage || en;
-	});
+  const { i18n, t } = useTranslation();
+  
+  const [language, setLanguage] = useState(() => {
+    return localStorage.getItem('selectedLanguage') || en;
+  });
 
-	const {i18n, t} = useTranslation();
+  useEffect(() => {
+    i18n.changeLanguage(language);
+  }, [language, i18n]);
 
-	const handleLanguageChange = async (lang: string) => {
-		setLanguage(lang);
-		localStorage.setItem('selectedLanguage', lang);
-		await i18n.changeLanguage(lang);
-	};
+  const handleLanguageChange = async (lang: string) => {
+    setLanguage(lang);
+    localStorage.setItem('selectedLanguage', lang);
+    await i18n.changeLanguage(lang);
+  };
 
-	return (
-		<ToggleGroup
+  return (
+		<><ToggleGroup
 			type="single"
 			value={language}
 			onValueChange={handleLanguageChange}
@@ -37,7 +40,8 @@ const LanguageSelector: React.FC = () => {
 				<div className="uppercase">ch</div>
 			</ToggleGroupItem>
 		</ToggleGroup>
-	);
+		</>
+  );
 };
 
 export default LanguageSelector;
