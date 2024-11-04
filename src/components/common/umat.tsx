9298d76a84d2fa9956026104a7d55685ce3dd69e
7imonn <timon.vandennobelen@novu.ch>
2024-11-04 15:14:48 +0100

    feat: add date

--- src/components/common/umat.tsx
+++ src/components/common/umat.tsx
@@ -8,16 +8,16 @@ type UmatProps = {
   createdAt?: Date;
 };
 
-const Umat: React.FC<UmatProps> = ({ createdBy, modifiedBy }) => (
+const Umat: React.FC<UmatProps> = ({ createdBy, modifiedBy, createdAt, modifiedAt }) => (
   <div>
     {(createdBy ?? modifiedBy) && (
       <div className='mt-5'>
         <p className='text-xs'>{t('infoTextUmat')}</p>
         {createdBy && (
-          <p className='text-xs'>{t('createdBy')}: {createdBy}</p>
+          <p className='text-xs'>{t('createdBy')}: {createdBy} {t('on')}: {createdAt?.toDateString()} </p>
         )}
         {modifiedBy && (
-          <p className='text-xs'>{t('modifiedBy')}: {modifiedBy}</p>
+          <p className='text-xs'>{t('modifiedBy')}: {modifiedBy} {t('on')}: {modifiedAt?.toDateString()}</p>
         )}
       </div>
     )}
