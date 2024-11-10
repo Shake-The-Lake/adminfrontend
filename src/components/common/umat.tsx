0c29a3bcd528a8cdbc4487185f1a16a14e5c9f65
7imonn <timon.vandennobelen@novu.ch>
2024-11-10 18:12:27 +0100

    feat: style umat stuff

--- src/components/common/umat.tsx
+++ src/components/common/umat.tsx
@@ -1,23 +1,26 @@
 import { t } from 'i18next';
+import { InfoIcon } from 'lucide-react';
 import React from 'react';
+import { toSwissLocalDateTimeString } from '../../lib/date-time.utils';
 
 type UmatProps = {
   createdBy?: string;
   modifiedBy?: string;
-  modifiedAt?: Date;
-  createdAt?: Date;
+  modifiedAt?: string;
+  createdAt?: string;
 };
 
 const Umat: React.FC<UmatProps> = ({ createdBy, modifiedBy, createdAt, modifiedAt }) => (
-  <div>
+  <div className='flex items-center w-full mb-5 border border-solid border-primary-stroke rounded-sm text-primary-dark-stroke'>
+    <InfoIcon className='size-5 mx-1'></InfoIcon>
     {(createdBy ?? modifiedBy) && (
-      <div className='mt-5'>
+      <div className='py-2 px-1'>
         <p className='text-xs'>{t('infoTextUmat')}</p>
         {createdBy && (
-          <p className='text-xs'>{t('createdBy')}: {createdBy} {t('on')}: {createdAt?.toDateString()} </p>
+          <p className='text-xs'>{t('createdBy')}: {createdBy} {t('on')}: {toSwissLocalDateTimeString(new Date(createdAt ?? ''))} </p>
         )}
         {modifiedBy && (
-          <p className='text-xs'>{t('modifiedBy')}: {modifiedBy} {t('on')}: {modifiedAt?.toDateString()}</p>
+          <p className='text-xs'>{t('modifiedBy')}: {modifiedBy} {t('on')}: {toSwissLocalDateTimeString(new Date(modifiedAt ?? ''))}</p>
         )}
       </div>
     )}
