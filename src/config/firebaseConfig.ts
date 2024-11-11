7e3578b01b9de4546f1cd1739b9d6e9b78af226d
sollberger.n@gmail.com <Noel Sollberger>
2024-11-11 20:27:23 +0100

    bugfix: linter type error

--- src/config/firebaseConfig.ts
+++ src/config/firebaseConfig.ts
@@ -1,18 +1,28 @@
-// src/config/firebaseConfig.ts
-import { initializeApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
+// Src/config/firebaseConfig.ts
+import {initializeApp} from 'firebase/app';
+import {getAuth} from 'firebase/auth';
 
-const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
-  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
+type FirebaseConfig = {
+	apiKey: string;
+	authDomain: string;
+	projectId: string;
+	storageBucket: string;
+	messagingSenderId: string;
+	appId: string;
+	measurementId?: string; // Optional property
+};
+
+const firebaseConfig: FirebaseConfig = {
+	apiKey: import.meta.env.VITE_FIREBASE_API_KEY as string,
+	authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN as string,
+	projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID as string,
+	storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET as string,
+	messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID as string,
+	appId: import.meta.env.VITE_FIREBASE_APP_ID as string,
+	measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID as string,
 };
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 
-export { auth };
+export {auth};
