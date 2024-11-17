9cef871c379e3ef4ed6659ca467f86aef9cb6b55
7imonn <timon.vandennobelen@novu.ch>
2024-11-17 21:57:46 +0100

    fix test

--- tests/App.test.tsx
+++ tests/App.test.tsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import App from '../src/App';
-import { describe, it } from 'vitest';
+import { describe, it, vi } from 'vitest';
 
-jest.mock('../src/config/firebaseConfig', () => ({
-	auth: {
-		signInWithEmailAndPassword: jest.fn(),
-		createUserWithEmailAndPassword: jest.fn(),
-		signOut: jest.fn(),
-		onAuthStateChanged: jest.fn(),
-	},
-}));
+vi.mock('../src/config/firebaseConfig', () => {
+	const auth = {
+		signInWithEmailAndPassword: vi.fn(),
+		createUserWithEmailAndPassword: vi.fn(),
+		signOut: vi.fn(),
+		onAuthStateChanged: vi.fn(),
+	};
+	return {
+		getAuth: () => auth,
+	};
+});
 
 describe('A truthy statement', () => {
 	it('renders the App component', () => {
