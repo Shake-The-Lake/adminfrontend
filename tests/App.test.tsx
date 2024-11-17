af7749384b61cc9e0aee79f68f095db32b481af2
7imonn <timon.vandennobelen@novu.ch>
2024-11-17 21:53:57 +0100

    fix: test

--- tests/App.test.tsx
+++ tests/App.test.tsx
@@ -3,6 +3,15 @@ import { render } from '@testing-library/react';
 import App from '../src/App';
 import { describe, it } from 'vitest';
 
+jest.mock('../src/config/firebaseConfig', () => ({
+	auth: {
+		signInWithEmailAndPassword: jest.fn(),
+		createUserWithEmailAndPassword: jest.fn(),
+		signOut: jest.fn(),
+		onAuthStateChanged: jest.fn(),
+	},
+}));
+
 describe('A truthy statement', () => {
 	it('renders the App component', () => {
 		render(<App />);
