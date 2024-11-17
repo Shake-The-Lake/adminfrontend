2ad24eb5ada14dfb7b8f7308034d8221b9add81e
7imonn <timon.vandennobelen@novu.ch>
2024-11-17 22:04:42 +0100

    fix: test

--- tests/App.test.tsx
+++ tests/App.test.tsx
@@ -3,18 +3,20 @@ import { render } from '@testing-library/react';
 import App from '../src/App';
 import { describe, it, vi } from 'vitest';
 
-vi.mock('../src/config/firebaseConfig', async () => {
-	const actual = await import('../src/config/firebaseConfig');
-	const auth = {
-		signInWithEmailAndPassword: vi.fn(),
-		createUserWithEmailAndPassword: vi.fn(),
-		signOut: vi.fn(),
-		onAuthStateChanged: vi.fn(),
-	};
+vi.mock('../src/config/firebaseConfig', async (importOriginal) => {
 	return {
-		...actual,
-		getAuth: () => auth,
-		auth, // Add this line to export auth
+		getAuth: () => ({
+			signInWithEmailAndPassword: vi.fn(),
+			createUserWithEmailAndPassword: vi.fn(),
+			signOut: vi.fn(),
+			onAuthStateChanged: vi.fn(),
+		}),
+		auth: {
+			signInWithEmailAndPassword: vi.fn(),
+			createUserWithEmailAndPassword: vi.fn(),
+			signOut: vi.fn(),
+			onAuthStateChanged: vi.fn(),
+		},
 	};
 });
 
