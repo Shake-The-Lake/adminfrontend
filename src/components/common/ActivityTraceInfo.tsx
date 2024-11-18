1ceb658af55ffb6aed30e20583678f45933d691e
Timon van den Nobelen <timon.vandennobelen@students.fhnw.ch>
2024-11-18 19:58:48 +0100

    Bugfix/rename base props

--- src/components/common/ActivityTraceInfo.tsx
+++ src/components/common/ActivityTraceInfo.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 import { toSwissLocalDateTimeString } from '../../lib/date-time.utils';
 import { type BaseModel } from '../../models/api/base.model';
 
-const ActivityTraceInfo: React.FC<BaseModel> = ({ createdBy, modifiedBy, createdAt, modifiedAt }) => (
+const ActivityTraceInfo: React.FC<BaseModel> = ({ createdBy, updatedBy, createdAt, updatedAt }) => (
   <div className='flex items-center w-full mb-5 border border-solid border-primary-stroke rounded-sm text-primary-dark-stroke'>
     <InfoIcon className='size-5 mx-1'></InfoIcon>
     {(createdBy ?? modifiedBy) && (
@@ -13,8 +13,8 @@ const ActivityTraceInfo: React.FC<BaseModel> = ({ createdBy, modifiedBy, created
         {(createdBy && createdAt) && (
           <p className='text-xs'>{t('createdBy')}: {createdBy} {t('on')}: {toSwissLocalDateTimeString(createdAt)} </p>
         )}
-        {(modifiedBy && modifiedAt) && (
-          <p className='text-xs'>{t('modifiedBy')}: {modifiedBy} {t('on')}: {toSwissLocalDateTimeString(modifiedAt)}</p>
+        {(updatedBy && updatedAt) && (
+          <p className='text-xs'>{t('modifiedBy')}: {updatedBy} {t('on')}: {toSwissLocalDateTimeString(updatedAt)}</p>
         )}
       </div>
     )}
