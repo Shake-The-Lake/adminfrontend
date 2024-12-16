6bfaf71d84e779281aacd0ce94f70ae9b8416ee1
Burger Alain (IT-PTR-CEN2-YPT) <alain_nicolas.burger@sbb.ch>
2024-12-16 13:39:32 +0100

    feat: add hover to timeslot items in schedule view

--- src/components/planby/program-item.tsx
+++ src/components/planby/program-item.tsx
@@ -1,19 +1,17 @@
-/* eslint-disable @typescript-eslint/no-unsafe-argument */
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import React from 'react';
 import {
 	ProgramBox,
 	ProgramContent,
 	ProgramFlex,
 	ProgramStack,
-	ProgramTitle,
 	ProgramText,
+	ProgramTitle,
 	useProgram,
 } from 'planby';
-import { Link } from 'react-router-dom';
-import { toSwissLocaleTimeString } from '../../lib/date-time.utils';
-import { useTranslation } from 'react-i18next';
-
+import {Link} from 'react-router-dom';
+import {toSwissLocaleTimeString} from '../../lib/date-time.utils';
+import {useTranslation} from 'react-i18next';
+import {HoverCard, HoverCardContent, HoverCardTrigger} from '../ui/hover-card';
 
 export type PlanByProgramItemProps = {
 	program: any;
@@ -23,15 +21,15 @@ export type PlanByProgramItemProps = {
 };
 
 export const ProgramItem: React.FC<PlanByProgramItemProps> = (props) => {
-	const { styles, isLive } = useProgram({
+	const {styles, isLive} = useProgram({
 		...props,
 		isBaseTimeFormat: props.isBaseTimeFormat ?? true,
 	});
 
-	const { t } = useTranslation();
+	const {t} = useTranslation();
 
-	const { data } = props.program;
-	const { title, since, till, color, disable } = data;
+	const {data} = props.program;
+	const {title, since, till, color, disable} = data;
 
 	const sinceTime = toSwissLocaleTimeString(new Date(since));
 	const tillTime = toSwissLocaleTimeString(new Date(till));
@@ -40,30 +38,45 @@ export const ProgramItem: React.FC<PlanByProgramItemProps> = (props) => {
 		<ProgramBox
 			width={styles.width}
 			style={styles.position}
-			className={'relative'}>
-			<ProgramContent
-				width={styles.width}
-				isLive={isLive}
-				className={disable ? '!cursor-default' : 'cursor-pointer'}
-				style={{ background: color }}>
-				<ProgramFlex>
-					<ProgramStack
-						className={'w-full'}>
-						{!disable && (
-							<Link
-								to={`${data.id}`}
-								className="after:absolute after:inset-0" />
-						)}
-						<ProgramTitle>{title}</ProgramTitle>
-						<div className="flex justify-between w-full">
-							<ProgramText>
-								{sinceTime} - {tillTime}
-							</ProgramText>
-							<ProgramText>{` R: ${data.seatsRider} / V: ${data.seatsViewer}`}</ProgramText>
-						</div>
-					</ProgramStack>
-				</ProgramFlex>
-			</ProgramContent>
+			className="relative">
+			<HoverCard>
+				<HoverCardTrigger asChild>
+					<ProgramContent
+						width={styles.width}
+						isLive={isLive}
+						className={disable ? '!cursor-default' : 'cursor-pointer'}
+						style={{background: color}}>
+						<ProgramFlex>
+							<ProgramStack className="w-full">
+								{disable ? null : (
+									<Link
+										to={`${data.id}`}
+										className="after:absolute after:inset-0"
+									/>
+								)}
+								<ProgramTitle>{title}</ProgramTitle>
+								<div className="flex justify-between w-full">
+									<ProgramText>
+										{sinceTime} - {tillTime}
+									</ProgramText>
+									<ProgramText>{` R: ${data.seatsRider} / V: ${data.seatsViewer}`}</ProgramText>
+								</div>
+							</ProgramStack>
+						</ProgramFlex>
+					</ProgramContent>
+				</HoverCardTrigger>
+				<HoverCardContent className="p-4 bg-white shadow-md rounded-md">
+					<h4 className="font-semibold mb-2">{title}</h4>
+					<p>
+						{t('From')}: {sinceTime} <br />
+						{t('To')}: {tillTime}
+					</p>
+					<p>
+						{t('Seats Riders')}: {data.seatsRider} <br />
+						{t('Seats Viewers')}: {data.seatsViewer}
+					</p>
+				</HoverCardContent>
+			</HoverCard>
 		</ProgramBox>
 	);
 };
